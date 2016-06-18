a534b15459d83733a4fba71e14ca5802ea7e7ed8
Vladimir Kuznetsov <mistakster@gmail.com>
2016-06-18 20:39:21 +0500

    improved test for builder

--- tests/builder-spec.js
+++ tests/builder-spec.js
@@ -6,14 +6,22 @@ var generator = require('../index');
 
 var b = generator('block');
 
-describe('block', function () {
-  it('should has builder', function () {
+describe('builder', function () {
+  it('should exists in block', function () {
     b.should.have.property('builder').Function();
 
     var builder = b.builder();
 
-    builder().should.eql('block');
+    builder().should.equal('block');
+
+  });
+
+  it('should has more methods', function () {
+    var builder = b.builder();
 
     builder.should.have.properties(['state', 'is', 'has', 'mix']);
-  });
+
+    builder('icon').is({'loading': true}).mix('icon')()
+      .should.equal('block__icon icon is-loading');
+  })
 });
